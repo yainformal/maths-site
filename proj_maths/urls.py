d6f9ee4216c62c77a73c5745294011b3328da6d7
dig-kafedra-webdev <128411381+dig-kafedra-webdev@users.noreply.github.com>
2023-03-21 22:55:24 +0300

    add django scripts

---
+++ proj_maths/urls.py
@@ -0,0 +1,26 @@
+"""proj_maths URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+    path('terms-list', views.terms_list),
+    path('add-term', views.add_term),
+    path('send-term', views.send_term),
+    path('stats', views.show_stats)
+]
