0821cb532290c1ad359c651c655d2fe70d49e7e5
Mikail Gubanov <yainformal@ya.ru>
2023-03-30 20:42:30 +0300

    write_term -- ссылка актуализирована

--- proj_maths/terms_work.py
+++ proj_maths/terms_work.py
@@ -11,7 +11,7 @@ def get_terms_for_table():
 
 def write_term(new_term, new_definition):
     new_term_line = f"{new_term};{new_definition};user"
-    with open("./proj_maths/data/terms.csv", "r", encoding="utf-8") as f:
+    with open("./data/terms.csv", "r", encoding="utf-8") as f:
         existing_terms = [l.strip("\n") for l in f.readlines()]
         title = existing_terms[0]
         old_terms = existing_terms[1:]
